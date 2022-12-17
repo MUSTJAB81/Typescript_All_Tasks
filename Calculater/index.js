1b16dc80ed6ce5ad32976d61e470c9e32ffeab66
SYED-MUSTJAB-HYDER <69319614+MUSTJAB81@users.noreply.github.com>
2022-12-17 05:23:09 +0500

    New

---
+++ Calculater/index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+// import { loading } from 'cli-loading-animation';
+// import spinners from 'cli-spinners';
+const stop_count = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function wellcome() {
+    let startAnimation = chalkAnimation.rainbow('Let Start My Calculator');
+    await stop_count();
+    startAnimation.stop();
+}
+await wellcome();
+async function askQuestion() {
+    const answers = await inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'operator',
+            message: 'Enter Your Operation to Perform ?\n',
+            choices: ['+ Addition', 'X Multiply', '% Divid', '- Subtract'],
+        },
+        {
+            type: 'number',
+            name: 'num1',
+            message: 'Enter Number 1 :',
+        },
+        {
+            type: 'number',
+            name: 'num2',
+            message: 'Enter Number 2 :',
+        },
+    ]);
+    if (answers.operator === '+ Addition') {
+        console.log(chalk.green(`${answers.num1} + ${answers.num2} = ${answers.num1 + answers.num2}`));
+    }
+    else if (answers.operator === 'X Multiply') {
+        console.log(chalk.green(`${answers.num1} * ${answers.num2} = ${answers.num1 * answers.num2}`));
+    }
+    else if (answers.operator === '% Divid') {
+        console.log(chalk.green(`${answers.num1} / ${answers.num2} = ${answers.num1 / answers.num2}`));
+    }
+    else if (answers.operator === '- Subtract') {
+        console.log(chalk.green(`${answers.num1} - ${answers.num2} = ${answers.num1 - answers.num2}`));
+    }
+}
+async function startAgain() {
+    do {
+        await askQuestion();
+        var again = await inquirer.prompt({
+            type: 'input',
+            name: 'startup',
+            message: 'Do you Want to Continue? Y or N',
+        });
+    } while (again.startup == 'y' || again.startup == 'Y' || again.startup == 'yes' || again.startup == 'Yes');
+}
+startAgain();
---
+++ number_guessing-game/index.ts
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+import chalkAnimation from 'chalk-animation'
+const sleep = () => new Promise((resolve, reject) => setTimeout(resolve, 2000))
+
+async function wellcome() {
+  let gameTitle = chalkAnimation.rainbow('Start Your Game........')
+  await sleep();
+  gameTitle.stop();
+}
+// wellcome()
+
+let playerLife = 3
+async function askQuiestion(){
+  let rdNum:number = Math.floor(Math.random() * 10 + 1)
+  do {
+      playerLife--;
+      console.log(chalk.blueBright(`The Life of Player left....${playerLife}`))
+      var que = await inquirer
+      .prompt([
+        {
+          type:"number",
+          name:"usr_num",
+          message: chalk.bgBlueBright("Enter Your Desier Number between 1-10 : "),
+          
+        }
+      ])
+      // console.log(que)
+      if (que.usr_num === rdNum) {
+        console.log(chalk.greenBright("Your Answer Is Correct 'awesome'....!"))
+      } else if (que.usr_num < rdNum) {
+        console.log(chalk.yellowBright(`Your Number ${que.usr_num} is less Than Guess Number `))
+      } else if (que.usr_num > rdNum) {
+        console.log(chalk.yellowBright(`Your Number ${que.usr_num} is greater Than Guess Number `))
+      }
+      
+      
+  } while ( playerLife>0 && rdNum !== que.usr_num);
+  if(playerLife===0  && rdNum !== que.usr_num){
+      console.log(chalk.red("Game is Over Know!"))
+  }
+
+ 
+}
+
+
+async function startAgain(){
+      do {
+            console.clear()
+            await wellcome()
+            playerLife =3;
+            await askQuiestion()
+            var restart= await inquirer.prompt([
+                  {
+                        type:"input",
+                        name:"start_again",
+                        message:chalk.bgGreenBright("Do You Want to Restart The Game? : Y or N :")
+                  }
+            ])
+            
+      } while (restart.start_again ==='y'||restart.start_again ==='Y'||restart.start_again ==='yes'||restart.start_again ==='YES');
+}
+startAgain()
