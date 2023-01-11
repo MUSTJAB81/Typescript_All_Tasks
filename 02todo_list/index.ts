415f0367f9e5e1e9add7d63e19848f159b2b1834
SYED-MUSTJAB-HYDER <69319614+MUSTJAB81@users.noreply.github.com>
2023-01-11 19:25:42 +0500

    New

---
+++ 02todo_list/index.ts
@@ -0,0 +1,80 @@
+import inquirer from 'inquirer'
+import chalk from 'chalk'
+import figlet from 'figlet'
+
+figlet('TODOLIST!!', function (err, data) {
+  if (err) {
+    console.log('Something went wrong...')
+    console.dir(err)
+    return
+  }
+  console.log(chalk.yellowBright(data))
+})
+
+let todoList: string[] = []
+
+async function repeatFlow() {
+  const answer = await inquirer.prompt([
+    {
+      name: 'repeat',
+      type: 'list',
+      choices: ['Yes', 'No'],
+      message: 'Do you want to other Operations?',
+    },
+  ])
+  return answer.repeat === 'Yes' ? true : false
+}
+
+async function TodoList() {
+  let startAgain = true
+
+  do {
+    const answer: { options: String } = await inquirer.prompt([
+      {
+        name: 'options',
+        type: 'list',
+        choices: ['Add Items', 'Display', 'Remove Items'],
+        message: 'What you want to do?',
+      },
+    ])
+
+    if (answer.options == 'Add Items') {
+      const item: { newItem: string } = await inquirer.prompt([
+        {
+          name: 'newItem',
+          type: 'input',
+          message: 'What item do you want to add New Item?',
+        },
+      ])
+      todoList.push(item.newItem)
+      startAgain = await repeatFlow()
+    } else if (answer.options == 'Display') {
+      if (todoList.length == 0) {
+        console.log('No items to display')
+      }
+      todoList.forEach((element) => console.log(element))
+      startAgain = await repeatFlow()
+    } else if (answer.options == 'Remove Items') {
+      if (todoList.length == 0) {
+        console.log('No items to display')
+      }
+      const removeItem: { remove: string } = await inquirer.prompt([
+        {
+          name: 'remove',
+          type: 'input',
+          message: 'Enter item to remove: ',
+        },
+      ])
+      let index = todoList.indexOf(removeItem.remove)
+
+      if (index !== -1) {
+        todoList.splice(index, 1)
+      }
+      console.log(index)
+      startAgain = await repeatFlow()
+    }
+  } while (startAgain !== false)
+}
+setTimeout(() => {
+  TodoList()
+}, 2000)
