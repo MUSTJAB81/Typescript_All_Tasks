48e8ce92cf9996a0da2f84be700647e20e59d27d
SYED-MUSTJAB-HYDER <69319614+MUSTJAB81@users.noreply.github.com>
2023-01-12 19:27:51 +0500

    New Change

--- 02todo_list/index.js
+++ 02todo_list/index.js
@@ -1,6 +1,6 @@
-import inquirer from "inquirer";
-import chalk from "chalk";
-import figlet from "figlet";
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import figlet from 'figlet';
 figlet('TODOLIST!!', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
@@ -11,48 +11,56 @@ figlet('TODOLIST!!', function (err, data) {
 });
 let todoList = [];
 async function repeatFlow() {
-    const answer = await inquirer.prompt([{
-            name: "repeat",
-            type: "list",
-            choices: ["Yes", "No"],
-            message: "Do you want to other Operations?",
-        }]);
-    return (answer.repeat === "Yes") ? true : false;
+    const answer = await inquirer.prompt([
+        {
+            name: 'repeat',
+            type: 'list',
+            choices: ['Yes', 'No'],
+            message: 'Do you want to other Operations?',
+        },
+    ]);
+    return answer.repeat === 'Yes' ? true : false;
 }
 async function TodoList() {
     let startAgain = true;
     do {
-        const answer = await inquirer.prompt([{
-                name: "options",
-                type: "list",
-                choices: ["Add Items", "Display", "Remove Items"],
-                message: "What you want to do?"
-            }]);
-        if (answer.options == "Add Items") {
-            const item = await inquirer.prompt([{
-                    name: "newItem",
-                    type: "input",
-                    message: "What item do you want to add New Item?"
-                }]);
+        const answer = await inquirer.prompt([
+            {
+                name: 'options',
+                type: 'list',
+                choices: ['Add Items', 'Display', 'Remove Items'],
+                message: 'What you want to do?',
+            },
+        ]);
+        if (answer.options == 'Add Items') {
+            const item = await inquirer.prompt([
+                {
+                    name: 'newItem',
+                    type: 'input',
+                    message: 'What item do you want to add New Item?',
+                },
+            ]);
             todoList.push(item.newItem);
             startAgain = await repeatFlow();
         }
-        else if (answer.options == "Display") {
+        else if (answer.options == 'Display') {
             if (todoList.length == 0) {
-                console.log("No items to display");
+                console.log('No items to display');
             }
-            todoList.forEach(element => console.log(element));
+            todoList.forEach((element) => console.log(element));
             startAgain = await repeatFlow();
         }
-        else if (answer.options == "Remove Items") {
+        else if (answer.options == 'Remove Items') {
             if (todoList.length == 0) {
-                console.log("No items to display");
+                console.log('No items to display');
             }
-            const removeItem = await inquirer.prompt([{
-                    name: "remove",
-                    type: "input",
-                    message: "Enter item to remove: "
-                }]);
+            const removeItem = await inquirer.prompt([
+                {
+                    name: 'remove',
+                    type: 'input',
+                    message: 'Enter item to remove: ',
+                },
+            ]);
             let index = todoList.indexOf(removeItem.remove);
             if (index !== -1) {
                 todoList.splice(index, 1);
